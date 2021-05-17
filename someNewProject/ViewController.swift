a8a74f7a4b6e56b7bc0f53772586c73a210f7372
Yuriy Sultanov <heduxar@mail.ru>
2021-05-17 22:04:23 +0500

    Initial Commit

---
+++ someNewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  someNewProject
+//
+//  Created by Юрий Султанов on 17.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
